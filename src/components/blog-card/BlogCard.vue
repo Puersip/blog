357065a657ef22a903e0e197106c4df49be8a11e
chentongsheng <chentongshengshiwo@outlook.com>
2023-10-01 11:29:06 +0800

    fix(homePage): 固定博客列表的最小长宽，防止文字溢出

--- src/components/blog-card/BlogCard.vue
+++ src/components/blog-card/BlogCard.vue
@@ -31,8 +31,10 @@ const props = defineProps({
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   display: flex;
   flex-direction: column; /* 将卡片竖直排列 */
-  width: 50vw; /* 占整个屏幕的三分之一 */
+  width: 50vw; /* 占整个屏幕的二分之一 */
+  min-width: 250px; /* 固定最小的宽度 */
   height: 25vh; /* 占四分之一的屏幕高度 */
+  min-height: 200px; /* 固定最小的高度 */
   box-sizing: border-box; /* 包含内边距和边框在内的盒模型 */
   transition: box-shadow 0.5s ease; /* 添加阴影变化的过渡效果，持续时间为0.5秒 */
 }
