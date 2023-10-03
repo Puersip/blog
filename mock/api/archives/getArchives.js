736cf4dc98b03f5f3d23e78ea51bc3c735253f63
chentongsheng <chentongshengshiwo@outlook.com>
2023-10-03 20:41:16 +0800

    fix(archives): 优化归档接口

--- mock/api/archives/getArchives.js
+++ mock/api/archives/getArchives.js
@@ -7,12 +7,12 @@ export default [
         code: 200,
         message: 'ok',
         data: [
-          { date: '2023-01-15', titles: ['Article 1 Title', 'hello1'] },
-          { date: '2023-02-20', titles: ['Article 2 Title', 'hello2'] },
-          { date: '2023-03-10', titles: ['Article 3 Title', 'hello3'] },
-          { date: '2023-04-15', titles: ['Article 1 Title', 'hello4'] },
-          { date: '2023-05-20', titles: ['Article 2 Title', 'hello5'] },
-          { date: '2023-06-10', titles: ['Article 3 Title', 'hello6'] },
+          { date: '2023-01', titles: ['Article 1 Title', 'hello1'] },
+          { date: '2023-02', titles: ['Article 2 Title', 'hello2'] },
+          { date: '2023-03', titles: ['Article 3 Title', 'hello3'] },
+          { date: '2023-04', titles: ['Article 1 Title', 'hello4'] },
+          { date: '2023-05', titles: ['Article 2 Title', 'hello5'] },
+          { date: '2023-06', titles: ['Article 3 Title', 'hello6'] },
           // Add more entries as needed
         ],
       };
