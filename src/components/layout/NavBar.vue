cce52d616f5ff334d2126144e53e437ac6d7ed83
chentongsheng <chentongshengshiwo@outlook.com>
2023-10-04 19:16:39 +0800

    feat(navbar): 增加导航栏的github链接

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="navbar">
-    <router-link to="/"><TheIcon icon="iconamoon:home-fill" />首页</router-link>
-    <router-link to="/category"
-      ><TheIcon icon="tabler:category-filled" />分类</router-link
-    >
-    <router-link to="/archives"
-      ><TheIcon icon="ic:round-view-timeline" />归档</router-link
-    >
-    <router-link to="/links"><TheIcon icon="ri:links-line" />友链</router-link>
+    <div class="navbar-left">{{ baseTitle }}</div>
+    <div class="navbar-center">
+      <router-link to="/"
+        ><TheIcon icon="iconamoon:home-fill" />首页</router-link
+      >
+      <router-link to="/category"
+        ><TheIcon icon="tabler:category-filled" />分类</router-link
+      >
+      <router-link to="/archives"
+        ><TheIcon icon="ic:round-view-timeline" />归档</router-link
+      >
+      <router-link to="/links"
+        ><TheIcon icon="ri:links-line" />友人帐</router-link
+      >
+    </div>
+    <div class="navbar-right">
+      <a :href="githubAddress" target="_blank" rel="noopener noreferrer">
+        <TheIcon icon="akar-icons:github-fill" />
+      </a>
+    </div>
   </div>
 </template>
 
@@ -15,30 +27,53 @@
 import { defineOptions } from 'vue';
 
 defineOptions({ name: 'NavBar' });
+
+const baseTitle = import.meta.env.VITE_TITLE;
+const githubAddress = import.meta.env.VITE_GITHUB;
 </script>
 
 <style scoped>
 .navbar {
   position: fixed;
   top: 0;
-  left: 0; /* 设置一个较大的 z-index 值，确保导航栏位于其他页面元素之上 */
-  z-index: 1000; /* 确保 navbar 在页面上其他元素的前面 */
+  left: 0;
+  z-index: 1000;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
   width: 100%;
   overflow: hidden;
   text-align: center; /* 文字居中 */
   background-color: #333;
 }
 
-.navbar a {
+.navbar-left {
+  flex: 0.2;
+  font-weight: bold;
+}
+
+.navbar-center {
+  display: flex;
+  flex: 0.6;
+  justify-content: center;
+}
+
+.navbar-right {
+  flex: 0.2;
+}
+
+.navbar a,
+.navbar-right a {
   display: inline-block;
   padding: 14px 16px;
   line-height: 21px;
   color: #f2f2f2;
   text-decoration: none;
-  transition: background-color 0.3s; /* 添加选择特效 */
+  transition: background-color 0.3s;
 }
 
-.navbar a:hover {
+.navbar a:hover,
+.navbar-right a:hover {
   color: black;
   background-color: #ddd;
 }
